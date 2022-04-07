d2204708a8de93de3117b9e804c573abf16a3850
CodeKagi <setshedikk12@gmail.com>
2022-04-07 18:17:10 +0200

    update(app) : wrote the logic to return only unique program types to be displayed on the listing page

--- src/app/modules/welcome/components/list-videos/list-videos.component.ts
+++ src/app/modules/welcome/components/list-videos/list-videos.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Video } from 'src/app/models/video';
+import { VideoData } from 'src/app/models/video-data';
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
   selector: 'app-list-videos',
@@ -10,20 +12,39 @@ import { Video } from 'src/app/models/video';
 export class ListVideosComponent implements OnInit {
   pageTitle: string = 'popular titles';
   videoDataSubscription: Subscription;
-  videoList: Video[];
   hasHttpError: boolean = false;
+  filteredProgramTypes: Video[];
+  videos: Video[] | Error;
 
-  videoType = [
-    {
-      route: 'movie'
-    },
-    {
-      route : 'series'
-    }
-   ]
-  constructor() { }
+  constructor(private videoService: VideoService) { }
 
   ngOnInit(): void {
+  //  this.getAllVideos();
+    this.videoService.filterProgramTypes().subscribe( response => {
+      this.filteredProgramTypes = response;
+      console.log("response filtered is",  this.filteredProgramTypes);
+    })
   }
+
+
+  // getAllVideos(): void {
+  //   try {
+  //     this.videoDataSubscription = this.videoService
+  //       .getAllVideoList()
+  //       .subscribe((response: VideoData) => {
+  //         if (response?.entries) {
+  //           const programTypes = response.entries.filter((videoData, index, myArray) => {
+  //             return myArray.map(videos => videos['programType']).indexOf(videoData['programType']) === index
+  //           })
+  //           this.filteredProgramTypes = programTypes;
+  //           console.log("program types are", this.filteredProgramTypes);
+  //         }
+  //       });
+  //   } catch (error) {
+  //     error.message = `MoviesComponent::getAllVideos() - ${error.message}`;
+  //     throw error;
+  //   }
+  // }
+
 
 }
--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -4,6 +4,7 @@ import { Video } from '../models/video';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { VideoData } from '../models/video-data';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,13 @@ export class VideoService {
       }
     }
     return this.httpClient.get<VideoData>(this.apiUrl, options);
+  }
+
+  filterProgramTypes(): Observable<Video[]> {
+     return this.httpClient.get<VideoData>(this.apiUrl).pipe(map((response) => {
+       return response.entries.filter((videoData, index, myArray) => {
+         return myArray.map(videos => videos['programType']).indexOf(videoData['programType']) === index
+       })
+     }))
   }
 }
