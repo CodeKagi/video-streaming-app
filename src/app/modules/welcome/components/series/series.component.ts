ed365498d7d03122b436e74febaf3292594d999b
CodeKagi <setshedikk12@gmail.com>
2022-04-07 07:22:33 +0200

    update(app) : created service to get list of series and also filter them,

--- src/app/modules/welcome/components/series/series.component.ts
+++ src/app/modules/welcome/components/series/series.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Video } from 'src/app/models/video';
+import { VideoData } from 'src/app/models/video-data';
+import { VideoService } from 'src/app/services/video.service';
 
 @Component({
   selector: 'app-series',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./series.component.scss']
 })
 export class SeriesComponent implements OnInit {
-  pageTitle: string = 'popular series';
-  constructor() { }
+  pageTitle: string;
+  videoDataSubscription: Subscription;
+  videoList: Video[];
+  hasHttpError: boolean = false;
+  seriesList: Video[];
+  constructor(private videoService: VideoService, private activatedRoute: ActivatedRoute, private router : Router) { }
 
   ngOnInit(): void {
   }
+
+  getAllVideos(): void {
+    try {
+      this.videoDataSubscription = this.videoService
+        .getAllVideoList()
+        .subscribe((response: VideoData) => {
+          if (response?.entries) {
+            this.videoList = response.entries;
+            this.filterMovies();
+          }
+        });
+    } catch (error) {
+      error.message = `MoviesComponent::getAllVideos() - ${error.message}`;
+      throw error;
+    }
+  }
+
+  filterMovies(): void {
+    const series = this.videoList
+      ?.filter((series: Video) => {
+        return series.programType === 'movie' && series.releaseYear >= 2010;
+      })
+      .filter((series: Video, index: number) => {
+        return index < 21;
+      })
+      .sort((a, b) => (a.title > b.title ? 1 : -1));
+      this.seriesList = series;
+      console.log("filtered movies are", this.seriesList);
+  }
+
+  getVideoThumbnail(series: Video) {
+		return series.images['Poster Art'].url;
+	}
 
 }
